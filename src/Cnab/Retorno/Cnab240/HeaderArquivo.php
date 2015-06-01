be8b487da5a4df6e6126aa7e337a020ce94dc4c6
root <sandro_bocon@yahoo.com.br>
2015-06-01 14:17:43 -0300

    Deixado de forma dinamica interpretar o codigo_cedente de codigo_cedente_dv caso ele venha com codigo_operacao ou nao

--- src/Cnab/Retorno/Cnab240/HeaderArquivo.php
+++ src/Cnab/Retorno/Cnab240/HeaderArquivo.php
@@ -31,4 +31,50 @@ class HeaderArquivo extends \Cnab\Format\Linha
         else
             return null;
     }
+
+    public function getCodigoCedente()
+    {
+        if ($this->existField('codigo_cedente')) {
+            //  Para CAIXA, identifica 2 padrões para retorno com e sem cod operação junto com o campo de codigo_cedente
+            if ($this->codigo_banco == 104) {
+                $codigo_cedente = str_pad($this->codigo_cedente, 12, '0', STR_PAD_LEFT);
+                // Padrão com cod operação  0OOOCCCCCCCC
+                if (substr($codigo_cedente, 1,1) == 0 and substr($codigo_cedente, 2,3) > 0 and substr($codigo_cedente, -6) > 0) {
+                    return substr($codigo_cedente, -8);
+                // Padrão sem cod operação e codigo_cedente no inicio  CCCCCD000000
+                } elseif (substr($codigo_cedente, 1,4) > 0 and substr($codigo_cedente, -6) == 0) {
+                    return str_pad(substr($codigo_cedente, 0,5), 8, '0', STR_PAD_LEFT);
+                // Caso contrario consideramos padrão sem cod operação 'padrão' 0000CCCCCCCC
+                } else {
+                    return $codigo_cedente;
+                }
+            } else {
+                return $this->codigo_cedente;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public function getCodigoCedenteDv()
+    {
+        var_dump($this->codigo_cedente_dv);
+        if ($this->existField('codigo_cedente_dv')) {
+            //  Para CAIXA, identifica 2 padrões para retorno com e sem cod operação junto com o campo de codigo_cedente
+            if ($this->codigo_banco == 104) {
+                $codigo_cedente = str_pad($this->codigo_cedente, 12, '0', STR_PAD_LEFT);
+                // Padrão sem cod operação e codigo_cedente no inicio  CCCCCD000000
+                if (substr($codigo_cedente, 1,4) > 0 and substr($codigo_cedente, -6) == 0) {
+                    return substr($codigo_cedente, 5,1);
+                // Caso contrario consideramos padrão generic
+                } else {
+                    return $this->codigo_cedente_dv;
+                }
+            } else {
+                return $this->codigo_cedente_dv;
+            }
+        } else {
+            return null;
+        }
+    }
 }
